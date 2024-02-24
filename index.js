83e3f03f62e67c4ad20439ef107236ff0c1ac8db
ADGandro <157210375+ADGandro@users.noreply.github.com>
2024-02-24 12:31:09 +0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,37 @@
+let text = (Basketball)
+
+function text (str){
+    console.log(str.length)
+}
+
+function text (uppercase){
+    console.log(uppercase.toUppercase())
+}
+
+function text (first){
+    console.log(first.charAt(0))
+}
+
+function text (index){
+    console.log(index.indexOf(k))
+}
+
+function text (double){
+    console.log(double.repeat(2))
+}
+
+function text (texti){
+    console.log(texti.slice(B, a, s))
+}
+
+function text (start){
+    console.log(start.includes(B))
+}
+
+function text (massive){
+    console.log(massive.split(", "))
+}
+
+function text (swith){
+    console.log(swith.replace(Volleyball))
+}
